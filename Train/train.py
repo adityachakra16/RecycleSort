d43db1d1f6902eb58e3461897e1de8cd9d7cf3d8
Aditya Chakraborty <31283807+adityachakra16@users.noreply.github.com>
2020-01-09 01:52:00 -0500

    Update train.py
    Added code to visualize training performance

--- Train/train.py
+++ Train/train.py
@@ -13,6 +13,7 @@ from keras.applications.mobilenet_v2 import preprocess_input
 import numpy as np
 from IPython.display import Image
 from keras.optimizers import Adam
+import matplotlib.pyplot as plt
 
 def prepare_data(train_folder, validation_folder, validation):
     datagen=ImageDataGenerator(preprocessing_function=preprocess_input) #included in our dependencies
@@ -65,7 +66,7 @@ def NN_model(base_model_name, freeze_layers, visaualize_layers):
 
     return model
 
-def train(model, epochs, train_data, validation_data, save_model, save_dir):
+def train(model, epochs, train_data, validation_data, save_model, save_dir, visualize):
 
     step_size_train=train_data.n//train_data.batch_size
     if validation_data:
@@ -82,12 +83,40 @@ def train(model, epochs, train_data, validation_data, save_model, save_dir):
         model.save(save_dir)
         f = open(save_dir + "recyclesort_labels.txt", "w")
         for label in train_data.classes:
-            f.write(label + "\n")
+            f.write(label)
         f.close()
 
+    if visualize:
+        visualize_training_performance(history)
+
+def visualize_training_performance(history):
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(2, 1, 1)
+    plt.plot(acc, label='Training Accuracy')
+    plt.plot(val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.ylabel('Accuracy')
+    plt.ylim([min(plt.ylim()),1])
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(2, 1, 2)
+    plt.plot(loss, label='Training Loss')
+    plt.plot(val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.ylabel('Cross Entropy')
+    plt.ylim([0,1.0])
+    plt.title('Training and Validation Loss')
+    plt.xlabel('epoch')
+    plt.show()
 
 if __name__ == '__main__':
 
     train_data, validation_data = prepare_data('C:\\BDBI\\Prototype\\Train\\Train_images', 'C:\\BDBI\\Prototype\\Train\\Val_images', True)
     model = NN_model('MobileNetV2',  100, visaualize_layers = True)
-    train(model, 1, train_data, validation_data, True, 'C:\\BDBI\\Prototype\\Train\\weights\\RecycleSort_weights.h5')
+    train(model, 1, train_data, validation_data, True, 'C:\\BDBI\\Prototype\\Train\\weights\\RecycleSort_weights.h5', visualize=True)
